4422ed21a1cc2c086bc2caf9d1cdd3eeb2be7ce5
Redivan Yoga <84635396+redivanyoga@users.noreply.github.com>
2022-12-24 12:51:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -101,6 +101,32 @@ sudo systemctl daemon-reload
 sudo systemctl enable misestmd
 sudo systemctl restart misestmd
 
+misestmd unsafe-reset-all
+
+# Enable state sync
+
+SNAP_RPC="https://e1.mises.site:443"
+
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/$MIS_FOLDER/config/config.toml
+
+# Start Service
+sudo systemctl start && sudo journalctl -fu $MIS -o cat $MIS
+
+echo -e "\e[1m\e[31m[!]\e[0m SETUP FINISHED"
+echo -e "\e[1m\e[31m[!]\e[0m STATE SYNC ESTIMATION CAN TAKE 15-30 MINS PLEASE WAITTING"
+echo -e "\e[1m\e[31m[!]\e[0m CEK JOURNAL sudo sudo journalctl -fu $MIS -o cat $MIS"
+# End
+
+
 echo -e "\e[1m\e[31m[!]\e[0m ========= SETUP FINISHED ========="
 echo -e "\e[1m\e[31m[!]\e[0m CEK JOURNAL sudo sudo journalctl -fu misesmtd -o cat"
 # End
